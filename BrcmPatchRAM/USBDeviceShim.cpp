2cc48a9f20cf91fc897f581f1dc22e84961c7489
the-darkvoid <rob@Robs-MBP.lan>
2015-09-11 18:20:20 +0400

    Removed test BrcmBluetooth_0a5c_21fb.kext

---
+++ BrcmPatchRAM/USBDeviceShim.cpp
@@ -0,0 +1,9 @@
+//
+//  USBDeviceShim.cpp
+//  BrcmPatchRAM
+//
+//  Created by Rob van Oostenrijk on 9/11/15.
+//
+//
+
+#include "USBDeviceShim.hpp"
---
+++ BrcmPatchRAM/USBDeviceShim.h
@@ -0,0 +1,14 @@
+//
+//  USBDeviceShim.hpp
+//  BrcmPatchRAM
+//
+//  Created by Rob van Oostenrijk on 9/11/15.
+//
+//
+
+#ifndef USBDeviceShim_hpp
+#define USBDeviceShim_hpp
+
+#include <stdio.h>
+
+#endif /* USBDeviceShim_hpp */
