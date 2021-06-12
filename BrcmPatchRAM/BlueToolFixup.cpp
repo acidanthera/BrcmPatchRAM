a1a2acde7c8522f826919c732b4ad3360d8934f2
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2021-06-11 23:19:45 -0400

    More fixes

--- BrcmPatchRAM/BlueToolFixup.cpp
+++ BrcmPatchRAM/BlueToolFixup.cpp
@@ -30,7 +30,6 @@ PRODUCT_NAME *ADDPR(selfInstance) = nullptr;
 
 IOService *BlueToolFixup::probe(IOService *provider, SInt32 *score) {
     ADDPR(selfInstance) = this;
-    setProperty("VersionInfo", kextVersion);
     setName("bluetooth");
     uint8_t bytes[] {0x00, 0x00, 0x00, 0x00};
     setProperty("transport-encoding", bytes, sizeof(bytes));
@@ -57,82 +56,50 @@ void BlueToolFixup::stop(IOService *provider) {
     IOService::stop(provider);
 }
 
-static const int kPathMaxLen = 1024;
 
 #pragma mark - Patches
 
 static const uint8_t kSkipUpdateFilePathOriginal[] = "/etc/bluetool/SkipBluetoothAutomaticFirmwareUpdate";
-static const uint8_t kSkipUpdateFilePathPatched[] = "/System/Library/CoreServices/boot.efi\0\0\0\0\0\0\0\0\0\0\0\0\0";
+static const uint8_t kSkipUpdateFilePathPatched[] = "/System/Library/CoreServices/boot.efi";
 
-static_assert(sizeof(kSkipUpdateFilePathOriginal) == sizeof(kSkipUpdateFilePathPatched), "patch size invalid");
-
-static const char blueToolPath[kPathMaxLen] = "/usr/sbin/BlueTool";
+static const char *blueToolPath = "/usr/sbin/BlueTool";
 
 static mach_vm_address_t orig_cs_validate {};
 
 #pragma mark - Kernel patching code
 
-template <size_t patchSize>
-static inline void searchAndPatch(const void *haystack,
-                                  size_t haystackSize,
-                                  const char (&path)[kPathMaxLen],
-                                  const uint8_t (&needle)[patchSize],
-                                  const uint8_t (&patch)[patchSize]) {
-    if (UNLIKELY(strncmp(path, blueToolPath, sizeof(blueToolPath)) == 0)) {
-        if (KernelPatcher::findAndReplace(const_cast<void *>(haystack), haystackSize, needle, patchSize, patch, patchSize)) {
-            DBGLOG(MODULE_SHORT, "patch succeeded");
-        } else {
-            DBGLOG(MODULE_SHORT, "patch failed");
-        }
-    }
+template <size_t findSize, size_t replaceSize>
+static inline void searchAndPatch(const void *haystack, size_t haystackSize, const char *path, const uint8_t (&needle)[findSize], const uint8_t (&patch)[replaceSize]) {
+   if (UNLIKELY(KernelPatcher::findAndReplace(const_cast<void *>(haystack), haystackSize, needle, findSize, patch, replaceSize)))
+       DBGLOG(MODULE_SHORT, "found string to patch at %s!", path);
 }
+
+
 
 #pragma mark - Patched functions
 
 // For Big Sur
-static void patched_cs_validate_page(vnode_t vp,
-                                     memory_object_t pager,
-                                     memory_object_offset_t page_offset,
-                                     const void *data,
-                                     int *arg4,
-                                     int *arg5,
-                                     int *arg6) {
-    char path[kPathMaxLen];
-    int pathlen = kPathMaxLen;
-    FunctionCast(patched_cs_validate_page, orig_cs_validate)(vp, pager, page_offset, data, arg4, arg5, arg6);
-    if (vn_getpath(vp, path, &pathlen) == 0) {
+static void patched_cs_validate_page(vnode_t vp, memory_object_t pager, memory_object_offset_t page_offset, const void *data, int *validated_p, int *tainted_p, int *nx_p) {
+    char path[PATH_MAX];
+    int pathlen = PATH_MAX;
+    FunctionCast(patched_cs_validate_page, orig_cs_validate)(vp, pager, page_offset, data, validated_p, tainted_p, nx_p);
+    if (vn_getpath(vp, path, &pathlen) == 0 && UNLIKELY(strcmp(path, blueToolPath) == 0)) {
         searchAndPatch(data, PAGE_SIZE, path, kSkipUpdateFilePathOriginal, kSkipUpdateFilePathPatched);
     }
 }
 
+
 #pragma mark - Patches on start/stop
 
 static void pluginStart() {
-    LiluAPI::Error error;
-    
     SYSLOG(MODULE_SHORT, "start");
     // There is no point in routing cs_validate_range, because this kext should only be running on Monterey+
     if (getKernelVersion() >= KernelVersion::Monterey) {
-        error = lilu.onPatcherLoad([](void *user, KernelPatcher &patcher){
-            DBGLOG(MODULE_SHORT, "patching cs_validate_page");
-            mach_vm_address_t kern = patcher.solveSymbol(KernelPatcher::KernelID, "_cs_validate_page");
-            
-            if (patcher.getError() == KernelPatcher::Error::NoError) {
-                orig_cs_validate = patcher.routeFunctionLong(kern, reinterpret_cast<mach_vm_address_t>(patched_cs_validate_page), true, true);
-                
-                if (patcher.getError() != KernelPatcher::Error::NoError) {
-                    SYSLOG(MODULE_SHORT, "failed to hook _cs_validate_page");
-                } else {
-                    DBGLOG(MODULE_SHORT, "hooked cs_validate_page");
-                }
-            } else {
-                SYSLOG(MODULE_SHORT, "failed to find _cs_validate_page");
-            }
+        lilu.onPatcherLoadForce([](void *user, KernelPatcher &patcher) {
+            KernelPatcher::RouteRequest csRoute = KernelPatcher::RouteRequest("_cs_validate_page", patched_cs_validate_page, orig_cs_validate);
+            if (!patcher.routeMultipleLong(KernelPatcher::KernelID, &csRoute, 1))
+                SYSLOG(MODULE_SHORT, "failed to route cs validation pages");
         });
-        
-        if (error != LiluAPI::Error::NoError) {
-            SYSLOG(MODULE_SHORT, "failed to register onPatcherLoad method: %d", error);
-        }
     }
 }
 
