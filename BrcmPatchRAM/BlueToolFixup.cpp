d1378ed36687aa1e1dd72b93af9a5bd57cd30ad8
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2021-06-13 15:58:29 -0400

    Add BlueToolFixup (#12)

---
+++ BrcmPatchRAM/BlueToolFixup.cpp
@@ -0,0 +1,120 @@
+//
+//  BlueToolFixup.cpp
+//  BrcmPatchRAM
+//
+//  Created by Dhinak G on 6/11/21.
+//
+
+
+#include <Headers/plugin_start.hpp>
+#include <Headers/kern_api.hpp>
+#include <Headers/kern_user.hpp>
+#include <Headers/kern_util.hpp>
+#include <Headers/kern_version.hpp>
+
+
+#define MODULE_SHORT "btlfx"
+
+
+class EXPORT BlueToolFixup : public IOService {
+    OSDeclareDefaultStructors(BlueToolFixup)
+public:
+    IOService *probe(IOService *provider, SInt32 *score) override;
+    bool start(IOService *provider) override;
+};
+
+OSDefineMetaClassAndStructors(BlueToolFixup, IOService)
+
+
+IOService *BlueToolFixup::probe(IOService *provider, SInt32 *score) {
+    return ADDPR(startSuccess) ? IOService::probe(provider, score) : nullptr;
+}
+
+bool BlueToolFixup::start(IOService *provider) {
+    if (!IOService::start(provider)) {
+        SYSLOG("init", "failed to start the parent");
+        return false;
+    }
+    setProperty("VersionInfo", kextVersion);
+    setName("bluetooth");
+    uint8_t bytes[] {0x00, 0x00, 0x00, 0x00};
+    setProperty("transport-encoding", bytes, sizeof(bytes));
+    registerService();
+    
+    return true;
+}
+
+
+#pragma mark - Patches
+
+static const uint8_t kSkipUpdateFilePathOriginal[] = "/etc/bluetool/SkipBluetoothAutomaticFirmwareUpdate";
+static const uint8_t kSkipUpdateFilePathPatched[] = "/System/Library/CoreServices/boot.efi";
+
+static const char *blueToolPath = "/usr/sbin/BlueTool";
+
+static mach_vm_address_t orig_cs_validate {};
+
+#pragma mark - Kernel patching code
+
+template <size_t findSize, size_t replaceSize>
+static inline void searchAndPatch(const void *haystack, size_t haystackSize, const char *path, const uint8_t (&needle)[findSize], const uint8_t (&patch)[replaceSize]) {
+   if (UNLIKELY(KernelPatcher::findAndReplace(const_cast<void *>(haystack), haystackSize, needle, findSize, patch, replaceSize)))
+       DBGLOG(MODULE_SHORT, "found string to patch at %s!", path);
+}
+
+
+
+#pragma mark - Patched functions
+
+// For Big Sur
+static void patched_cs_validate_page(vnode_t vp, memory_object_t pager, memory_object_offset_t page_offset, const void *data, int *validated_p, int *tainted_p, int *nx_p) {
+    char path[PATH_MAX];
+    int pathlen = PATH_MAX;
+    FunctionCast(patched_cs_validate_page, orig_cs_validate)(vp, pager, page_offset, data, validated_p, tainted_p, nx_p);
+    if (vn_getpath(vp, path, &pathlen) == 0 && UNLIKELY(strcmp(path, blueToolPath) == 0)) {
+        searchAndPatch(data, PAGE_SIZE, path, kSkipUpdateFilePathOriginal, kSkipUpdateFilePathPatched);
+    }
+}
+
+
+#pragma mark - Patches on start/stop
+
+static void pluginStart() {
+    SYSLOG(MODULE_SHORT, "start");
+    // There is no point in routing cs_validate_range, because this kext should only be running on Monterey+
+    if (getKernelVersion() >= KernelVersion::Monterey) {
+        lilu.onPatcherLoadForce([](void *user, KernelPatcher &patcher) {
+            KernelPatcher::RouteRequest csRoute = KernelPatcher::RouteRequest("_cs_validate_page", patched_cs_validate_page, orig_cs_validate);
+            if (!patcher.routeMultipleLong(KernelPatcher::KernelID, &csRoute, 1))
+                SYSLOG(MODULE_SHORT, "failed to route cs validation pages");
+        });
+    }
+}
+
+// Boot args.
+static const char *bootargOff[] {
+    "-btlfxoff"
+};
+static const char *bootargDebug[] {
+    "-btlfxdbg"
+};
+static const char *bootargBeta[] {
+    "-btlfxbeta"
+};
+
+// Plugin configuration.
+PluginConfiguration ADDPR(config) {
+    xStringify(PRODUCT_NAME),
+    parseModuleVersion(xStringify(MODULE_VERSION)),
+    LiluAPI::AllowNormal | LiluAPI::AllowInstallerRecovery | LiluAPI::AllowSafeMode,
+    bootargOff,
+    arrsize(bootargOff),
+    bootargDebug,
+    arrsize(bootargDebug),
+    bootargBeta,
+    arrsize(bootargBeta),
+    KernelVersion::Monterey,
+    KernelVersion::Monterey,
+    pluginStart
+};
+
